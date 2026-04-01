732b8d293824946922f7a8b4c2e345ccbd7b7845
xoxoisme <kxoxo3333@gmail.com>
2026-04-01 22:44:47 +0900

    add: baekjoon 2171

--- CP/Baekjoon/Gold/2171-직사각형의 개수.py
+++ CP/Baekjoon/Gold/2171-직사각형의 개수.py
@@ -1,46 +1,46 @@
 import sys
 input = sys.stdin.readline
 
-n=int(input())
+n = int(input())
 
-l=[]
-nums=set()
-index=0
+l = []
+nums = set()
+index = 0
 for i in range(n):
-    x,y=map(int,input().split())
+    x,y = map(int,input().split())
     l.append([x,y])
     nums.add(x)
     nums.add(y)
-nums=sorted(list(nums))
-dnums={}
+nums = sorted(list(nums))
+dnums = {}
 for a in range(len(nums)):
-    dnums[nums[a]]=a
+    dnums[nums[a]] = a
  
-dx={}
-dy={}
+dx = {}
+dy = {}
 for i in l:
-    x=dnums[i[0]]
-    y=dnums[i[1]]
+    x = dnums[i[0]]
+    y = dnums[i[1]]
     if x in dx:
         dx[x].append(y)
     else:
-        dx[x]=[y]
+        dx[x] = [y]
     if y in dy:
         dy[y].append(x)
     else:
-        dy[y]=[x]
+        dy[y] = [x]
 
 
-count=0
+count = 0
 for i in dx:
-    if len(dx[i])>=2:
-        for j in range(len(dx[i])-1) :
-            for j2 in range(j+1,len(dx[i])):
-                p1=dx[i][j]  
-                p2=dx[i][j2] 
+    if len(dx[i]) >= 2:
+        for j in range(len(dx[i]) - 1) :
+            for j2 in range(j + 1,len(dx[i])):
+                p1 = dx[i][j]  
+                p2 = dx[i][j2] 
                     
                 for k in dy[p1]:
-                    if k>i:
+                    if k > i:
                         if k in dy[p2]:
-                            count+=1
+                            count += 1
 print(count)   
