61983926df05eb9a9b9da2e79f35ad7762d859c9
xoxoisme <kxoxo3333@gmail.com>
2026-04-02 20:17:59 +0900

    add: 1240

--- CP/Baekjoon/Gold/1240-노드 사이의 거리.py
+++ CP/Baekjoon/Gold/1240-노드 사이의 거리.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 def bfs(start,find):
     queue = deque()
     queue.append((start,0))
-    visited = [False] * (n+1)
+    visited = [False] * (n + 1)
     visited[start] = True
     while queue:
         v, d = queue.popleft()
@@ -16,16 +16,16 @@ def bfs(start,find):
         for i, l in graph[v]:
             if not visited[i]:
                 visited[i] = True
-                queue.append((i,d+l))
+                queue.append((i, d + l))
     
 n, m = map(int,input().split())
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 
-for _ in range(n-1):
+for _ in range(n - 1):
     n1, n2, l = map(int,input().split())
     graph[n1].append((n2,l))
     graph[n2].append((n1,l))
 
 for _ in range(m):
     n1, n2 = map(int,input().split())
-    print(bfs(n1,n2))
+    print(bfs(n1, n2))
