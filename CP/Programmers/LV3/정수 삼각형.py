9698e9d8ee40305139a6a15e959ba043268ddbb5
xoxoisme <kxoxo3333@gmail.com>
2026-06-16 18:47:52 +0900

    add: 정수 삼각형 풀이과정 추가

--- CP/Programmers/LV3/정수 삼각형.py
+++ CP/Programmers/LV3/정수 삼각형.py
@@ -4,3 +4,13 @@ def solution(triangle):
         for j in range(len(triangle[i])):
             triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
     return triangle[0][0]
+
+# 모든 값을 비교해야하기에 bottom-up 방식
+# ---
+# 트라이앵글을 table로 활용(table 말고 그냥 triangle 자체로 해도 될 거 같아서 바꿈)
+
+# 3행부터 계산해야하기에 range(len(triangle)-2, -1, -1) 반복 - i
+#     해당 3행 쭉 돌 때까지 반복 - j
+#         table[i][j] += (table[i+1][j], table[i+1][j+1]) 중에서 더 큰 값
+    
+# table[0][0]이 최대값
