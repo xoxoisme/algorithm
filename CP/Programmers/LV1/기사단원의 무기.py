05cdfddbfd2befb7768691d70ccb687dd21b2741
xoxoisme <kxoxo3333@gmail.com>
2026-06-26 00:20:24 +0900

    add: 프로그래머스 기사단원의 무기

--- CP/Programmers/LV1/기사단원의 무기.py
+++ CP/Programmers/LV1/기사단원의 무기.py
@@ -1,3 +1,9 @@
 def solution(number, limit, power):
-    res_list = [len([j for j in range(1, i+1) if i%j == 0]) for i in range(1, number+1)]
+    def cnt_div(i):
+        cnt = 0
+        for j in range(1, int(i**0.5)+1):
+            if i%j == 0:
+                cnt+= 2 if j != i//j else 1
+        return cnt
+    res_list = [cnt_div(i) for i in range(1, number+1)]
     return sum([i if i <= limit else power for i in res_list])
