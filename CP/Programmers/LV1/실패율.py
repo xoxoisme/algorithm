0d9b135c98db05cec15a0d12a645839487ec1618
xoxoisme <kxoxo3333@gmail.com>
2026-06-27 15:27:38 +0900

    add: 프로그래머스 실패율

--- CP/Programmers/LV1/실패율.py
+++ CP/Programmers/LV1/실패율.py
@@ -1,8 +1,11 @@
+from collections import Counter
+
 def solution(N, stages):
     res = []
+    counter = Counter(stages)
     n = len(stages)
     for i in range(1, N+1):
-        cnt = stages.count(i)
+        cnt = counter[i]
         if n == 0: res.append(0)
         else: res.append(cnt/n)
         n -= cnt
