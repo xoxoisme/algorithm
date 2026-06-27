177b176a8fe4d3b3738cffc9251e06e45bae0317
xoxoisme <kxoxo3333@gmail.com>
2026-06-27 13:46:59 +0900

    add: 프로그래머스 소수 찾기

---
+++ CP/Programmers/LV1/소수 찾기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    if n < 2: return 0
+    is_prime = [True]*(n+1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(1, int(n**0.5)+1):
+        if is_prime[i]:
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
+    return sum(is_prime)
