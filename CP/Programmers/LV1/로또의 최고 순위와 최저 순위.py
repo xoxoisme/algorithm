c2f2d5b54252b97a84372b35e739707c74b49a7f
xoxoisme <kxoxo3333@gmail.com>
2026-06-27 16:46:53 +0900

    add: 프로그래머스 로또의 최고 순위와 최저 순

---
+++ CP/Programmers/LV1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,7 @@
+def solution(lottos, win_nums):
+    rank = {6:1, 5:2, 4:3, 3:4, 2:5}
+    hope, win_nums, cnt = lottos.count(0), set(win_nums), 0
+    for i in range(len(lottos)):
+        if lottos[i] in win_nums: cnt += 1
+    hope += cnt
+    return [rank.get(hope, 6), rank.get(cnt, 6)]
